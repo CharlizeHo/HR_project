5eb7e2661b2f5ada0efa2df87d5093ea82f43cfa
VHweaboo <nguyenvohoang132001@gmail.com>
2023-06-01 23:02:24 +0700

    fix

--- Manager/Manager/src/main/java/com/example/Manager/Controller/UserController.java
+++ Manager/Manager/src/main/java/com/example/Manager/Controller/UserController.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth/UserCol")
-@CrossOrigin("http://localhost:3000/")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
